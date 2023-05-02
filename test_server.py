83ab6aea59deafc1f40c9644d21bf255020ef862
Noah Diana <noah.diana10312005@gmail.com>
2023-05-02 07:20:24 -0400

    working on test of CRUD

--- test_server.py
+++ test_server.py
@@ -1,4 +1,10 @@
 import requests
 
+# Retreives item by id
 print(requests.get("http://127.0.0.1:8000/items/0").json())
+
+# Retreives item by name
 print(requests.get("http://127.0.0.1:8000/items?name=Chocolate_bar").json())
+
+# Adds a new item 
+print(requests.post("http://127.0.0.1:8000/", json={"name": "Crunch_bar", "price": 3.99, "count": 23, "id": 3, "category": "candy"}).json())
